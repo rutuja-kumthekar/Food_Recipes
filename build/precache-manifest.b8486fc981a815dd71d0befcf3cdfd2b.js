27e1477348b99c2a6476d679ff458b276a0e1677
Rutuja Kumthekar <64900490+rutuja-kumthekar@users.noreply.github.com>
2022-08-29 22:07:31 +0530

    Add files via upload

---
+++ build/precache-manifest.b8486fc981a815dd71d0befcf3cdfd2b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "593522e916bd25e6a6c5",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/css/main.f4bf1252.chunk.css"
+  },
+  {
+    "revision": "593522e916bd25e6a6c5",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/js/main.b6cd619c.chunk.js"
+  },
+  {
+    "revision": "9494cb1fbe71474e718e",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/js/runtime~main.29b7b116.js"
+  },
+  {
+    "revision": "4c588e60b049f2b12675",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/css/2.42e02b61.chunk.css"
+  },
+  {
+    "revision": "4c588e60b049f2b12675",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/js/2.473baa0b.chunk.js"
+  },
+  {
+    "revision": "cc3fff7e1fb8fcdf7ec881e0a394ee72",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/logo.cc3fff7e.svg"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "badc49bc1071789376c70c9a650d3d5e",
+    "url": "/rutuja-kumthekar/Food_Recipes-App.git/index.html"
+  }
+];
